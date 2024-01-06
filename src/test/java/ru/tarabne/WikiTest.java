dd8d7afb742a8c5d8ee6bda6b00bedf17914245e
Nikita Tarabukhin <tarabne@gmail.com>
2024-01-06 21:30:52 +0300

    added jUnit5ExampleSearch test

---
+++ src/test/java/ru/tarabne/WikiTest.java
@@ -0,0 +1,43 @@
+package ru.tarabne;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WikiTest {
+
+    @BeforeAll
+    static void testConfiguration() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 5000;
+    }
+
+    @Test
+    void jUnit5ExampleSearch() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body").$("ul").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+        sleep(5000);
+
+    }
+}
